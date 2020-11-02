1a0fe79a13c76bb8f2aab47cac3dffa092a7141d
Marco Bassaletti <bassaletti@gmail.com>
2020-11-01 23:15:15 -0300

    Show similar posts on post detail.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.views.generic import ListView
 from taggit.models import Tag
 from .models import Post
@@ -37,7 +38,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    # List of similar posts
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {
+        'post': post, 
+        'comments': comments, 
+        'new_comment': new_comment, 
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
+        })
 
 
 def post_share(request, post_id):

6707c1d192188ddc97a7c48d6c9849d14f8a1aba
Marco Bassaletti <bassaletti@gmail.com>
2020-11-05 00:27:22 -0300

    Adding sitemap.

--- djangopress/settings.py
+++ djangopress/settings.py
@@ -34,6 +34,7 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
+SITE_ID = 1
 
 # Application definition
 
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
     'taggit'
 ]
